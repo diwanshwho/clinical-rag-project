0c2b4cba45003d435124163096fb710a930a5fcf
Diwanshu Sharma <132468837+diwanshwho@users.noreply.github.com>
2026-08-11 11:18:33 +0530

    added data sources details

---
+++ sources.py
@@ -0,0 +1,38 @@
+'''
+Registry of clinical guideline source documents.
+All sources are free/open-licensed and hosted on NCBI Bookshelf (nih.gov).
+'''
+
+SOURCES = [
+    {
+        'disease': 'hypertension',
+        'title': 'WHO Guideline for the Pharmacological Treatment of Hypertension in Adults',
+        'year': 2021,
+        'publisher': 'World Health Organization',
+        'pdf_url': 'https://www.ncbi.nlm.nih.gov/books/n/who344424/pdf/',
+        'source_page': 'https://www.ncbi.nlm.nih.gov/books/NBK573631/',
+        'filename': 'hypertension_who_2021.pdf',
+    },
+    {
+        'disease': 'diabetes',
+        'title': (
+            'WHO Guidelines on Second- and Third-line Medicines and Type of '
+            'Insulin for the Control of Blood Glucose Levels in Non-Pregnant '
+            'Adults with Diabetes Mellitus'
+        ),
+        'year': 2018,
+        'publisher': 'World Health Organization',
+        'pdf_url': 'https://www.ncbi.nlm.nih.gov/books/n/who272433/pdf/',
+        'source_page': 'https://www.ncbi.nlm.nih.gov/books/NBK532229/',
+        'filename': 'diabetes_who_2018.pdf',
+    },
+    {
+        'disease': 'asthma',
+        'title': 'Expert Panel Report 3: Guidelines for the Diagnosis and Management of Asthma',
+        'year': 2007,
+        'publisher': 'National Heart, Lung, and Blood Institute (NHLBI/NIH)',
+        'pdf_url': 'https://www.ncbi.nlm.nih.gov/books/n/asthma3/pdf/',
+        'source_page': 'https://www.ncbi.nlm.nih.gov/books/NBK7232/',
+        'filename': 'asthma_nhlbi_2007.pdf',
+    },
+]
